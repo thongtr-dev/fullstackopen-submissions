1568d510e349136cc16c8b7250a2f63d43cf6cd4
thongtr-dev <thongtr.dev@gmail.com>
2023-07-18 00:25:59 +0700

    2.11: phonebook

--- src/App.js
+++ src/App.js
@@ -1,16 +1,75 @@
-import Note from "./components/Note";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Filter from "./components/Filter";
+import Form from "./components/Form";
+import Contacts from "./components/Contacts";
 
-const App = ({ notes }) => {
+const App = () => {
+  const [contacts, setContacts] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3001/persons")
+      .then((res) => setContacts(res.data));
+  }, []);
+  const [newName, setNewName] = useState("");
+  const [newPhoneNumber, setNewPhoneNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
+  const handleOnChange = (state) => {
+    if (state === "name") return (event) => setNewName(event.target.value);
+    if (state === "phone")
+      return (event) => setNewPhoneNumber(event.target.value);
+    if (state === "filter") return (event) => setFilter(event.target.value);
+  };
+
+  const addContact = (event) => {
+    event.preventDefault();
+    if (!newName.length || !newPhoneNumber.length) {
+      window.alert("Please fill in the required fields.");
+      return;
+    }
+    const validatedName = newName
+      .toLowerCase()
+      .trim()
+      .split(" ")
+      .filter((char) => char !== "")
+      .join(" ");
+    const contactExist = contacts.some(
+      (contact) => contact.name.toLowerCase() === validatedName
+    );
+    if (contactExist) {
+      window.alert(`${newName} is already added to phonebook.`);
+      return;
+    }
+
+    const phoneNumberPattern =
+      /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im;
+
+    if (!phoneNumberPattern.test(newPhoneNumber)) {
+      window.alert(
+        `Invalid phone number "${newPhoneNumber}". Please try again.`
+      );
+      return;
+    }
+
+    setContacts(contacts.concat({ name: newName, phone: newPhoneNumber }));
+    setNewName("");
+    setNewPhoneNumber("");
+  };
+
   return (
     <div>
-      <h1>Notes</h1>
-      <ul>
-        <ul>
-          {notes.map((note) => (
-            <Note key={note.id} note={note} />
-          ))}
-        </ul>
-      </ul>
+      <h2>Phonebook</h2>
+      <Filter filter={filter} handleChange={handleOnChange} />
+      <h3>Add a new</h3>
+      <Form
+        handleSubmit={addContact}
+        newName={newName}
+        newPhoneNumber={newPhoneNumber}
+        handleChange={handleOnChange}
+      />
+      <h3>Numbers</h3>
+      <Contacts contacts={contacts} filter={filter} />
     </div>
   );
 };
---
+++ src/components/Contacts.js
@@ -0,0 +1,22 @@
+const Contact = ({ contact }) => (
+  <p>
+    {contact.name} {contact.number}
+  </p>
+);
+
+const Contacts = ({ contacts, filter }) => {
+  if (contacts.length) {
+    if (filter.length) {
+      return contacts
+        .filter((contact) =>
+          contact.name.toLowerCase().includes(filter.toLowerCase())
+        )
+        .map((contact) => <Contact key={contact.name} contact={contact} />);
+    }
+    return contacts.map((contact) => (
+      <Contact key={contact.id} contact={contact} />
+    ));
+  } else return "...";
+};
+
+export default Contacts;
